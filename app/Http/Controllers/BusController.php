d95dd370dbb2098fe4087266f4726460ba384d7c
Nathan Woodhouse <nathan@calibraelearning.com>
2022-03-17 17:26:27 +0000

    Added some more helpful comments

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 
 class BusController extends Controller
 {
-    // fake DB stud...
+    // fake DB stub. The index of the item in the array will serve as an 'id' for the item.
     private $buses = [
         '#19 Aldersham road', // 0
         '#23 Newport road',   // 1
         '#30x Newport express'// 2
     ];
 
-    // method = 'action'
     public function showForm() {
         return view('bus.form');
     }
@@ -26,12 +25,17 @@ class BusController extends Controller
         return view('bus-result', compact('name', 'message'));
     }
 
+    /**
+     * List an index of all bus routes
+     */
     public function index() {
         $buses = $this->buses;
-
         return view('bus.index', compact('buses'));
     }
 
+    /**
+     * Show details for a particular bus
+     */
     public function show($id) {
 
         // pretend database lookup
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 1. Register route, 
-// 2. define a controller with action, 
-// 3.hit the route
 Route::get('/form', [BusController::class, 'showForm']);
 
 Route::get('/bus-info', [BusController::class, 'showInfo']);
